2a5f653f5c225bb4f7d9c03b0982437f3c103487
JavaScriptDude <tim.c.quinn@gmail.com>
2020-09-21 23:20:29 -0400

    Adding Text Table Output
    Still have json output with --json (-j) switch

--- get_hosts.py
+++ get_hosts.py
@@ -1,12 +1,12 @@
 #########################################
 # .: get_hosts.py :.
 # Scans a /24 network or smaller to grab host information including IP, Mac and NIC Vendor
-# Returns as data JSON
+# Returns as text table or json with optional --json flag
 # .: Usage (linux) - sudo required :.
 # sudo ./get_hosts.sh $HOME 3.7.7 192.168.101.0/24
+# where: 3.7.7 is your pyenv version and 192.168.101.0/24 is the network to scan
 # Note: get_hosts.sh assumes you are using pyenv to manage your python versions.
 #       alter get_hosts.sh as required for other installations
-# where: 3.7.7 is your pyenv version and 192.168.101.0/24 is the network to scan
 # .: Usage (windows) - sudo not required :.
 # python get_hosts.py 192.168.101.0/24
 # .: Dependencies :.
@@ -17,40 +17,46 @@
 # Licence: https://opensource.org/licenses/MIT
 #########################################
 
-import sys, nmap, json, ipaddress
+import sys, nmap, json, ipaddress,argparse
 
-def printUsage(s):
-    print("Usage: python get_hosts.py <network>")
-    if not s is None: print(s)
+argp = argparse.ArgumentParser(prog="get_hosts")
+
+def printUsage(s: str):
+    argp.print_help(s)
+    # print("Usage: python get_hosts.py <network>")
+    # if not s is None: print(s)
     sys.exit(0)
 
-def main(argv):
-    nm = nmap.PortScanner()
+def main():
+
+    argp.add_argument("-j", "--json", action='store_true')
+    argp.add_argument("network", type=str)
+    args = argp.parse_args()
+
     # Validate args
-    if len(argv) == 1 and not argv[0].strip() == '':
-        address=argv[0]
+    if not args.network.strip() == '':
         try:
-            ipa = ipaddress.ip_network(address, strict=True)
+            ipa = ipaddress.ip_network(args.network, strict=True)
         except:
-            printUsage("Invalid address passed: " + address)
+            printUsage("Invalid address passed: " + args.network)
 
         if not ipa.version == 4: 
             printUsage("Only IPV4 is supported at this time")
         elif ipa.num_addresses > 256: 
             printUsage("Please only specify a /24 network or smaller")  
     else:
-        printUsage()
+        printUsage("Please pass a network")
     
     # execute nmap scan
-    nm.scan(hosts=address, arguments='-sP')
+    nm = nmap.PortScanner()
+    nm.scan(hosts=args.network, arguments='-sP')
 
     lHosts=[]
-    mRet = {'network': address, 'hosts': lHosts}
+    mRet = {'network': args.network, 'hosts': lHosts}
 
     # Scrape data
     for ip in nm.all_hosts():
         host = nm[ip]
-        print(host)
         mac = "-"
         vendorName = "-"
         if 'mac' in host['addresses']:
@@ -63,9 +69,21 @@ def main(argv):
         
         lHosts.append(rHost)
 
-    # dump output as JSON
-    print(json.dumps(mRet))
+    if args.json:
+        # dump output as JSON
+        print(json.dumps(mRet))
+    else:
+        import texttable as tt
+        header1 = ['ip', 'mac', 'vendor', 'status']
+        tbl = tt.Texttable(max_width=120)
+        tbl.set_cols_align(list('r'*len(header1)))
+        tbl.set_cols_dtype(list('t'*len(header1)))
+        tbl.set_deco(tbl.HEADER | tbl.VLINES)
+        tbl.header(header1)
+        for rHost in mRet['hosts']:
+            tbl.add_row([rHost['ip'], rHost['mac'], rHost['vendor'], rHost['status']])
+        print(tbl.draw())
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
--- get_hosts.sh
+++ get_hosts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Usage: % sudo ./get_hosts.sh $HOME 3.7.7 192.168.101.0/24
 # where: 3.7.7 is your pyenv version and 192.168.101.0/24 is the network to scan
+# Json Usage: % sudo ./get_hosts.sh $HOME 3.7.7 --json 192.168.101.0/24
 if ! ([ ${EUID:-$(id -u)} -eq 0 ]) ; then
     echo 'Sorry, you can only run as root'
     exit 0
@@ -8,4 +9,4 @@ fi
 export HOME=$1
 export PATH=$HOME/.pyenv/versions/$2/bin/:$PATH
 
-python get_hosts.py $3
+python get_hosts.py $3 $4
